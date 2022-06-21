b747bebf579fa66e45df5a9f38549157fd96d21e
TineCrnugelj <tine.crnugelj@gmail.com>
2022-06-21 17:08:59 +0200

    checkpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import { Fragment } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>   
-    </div>
+    <Fragment>
+      <Router>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
